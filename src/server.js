8d498147bfda9f7d7ab6bb9696d53bf7fd3877bd
guy biton <guy.biton@booking.com>
2020-06-13 13:57:08 +0200

    Refactor rendering initial app from server.js to renderInitialApp

--- src/server.js
+++ src/server.js
@@ -1,12 +1,7 @@
 import express from 'express';
-import React from 'react';
 import mongoose from 'mongoose';
-import { StaticRouter } from 'react-router-dom';
-import { renderToString } from 'react-dom/server';
-import { Provider } from 'react-redux';
 
-import { createStoreForClient } from "./server/getDerivedState";
-import App from "./App";
+import { getContext, createHTML } from "./server/renderInitialApp";
 import api from './server/api';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
@@ -28,45 +23,12 @@ server
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .use('/api', api)
   .get('/*', async (req, res) => {
-    const { store, context, initialState } = await createStoreForClient();
-    const markup = renderToString(
-      <Provider store={store}>
-        <StaticRouter context={context} location={req.url}>
-          <App />
-        </StaticRouter>
-      </Provider>
-    );
-
+    const context = getContext();
+    const html = await createHTML(assets, req);
     if (context.url) {
       res.redirect(context.url);
     } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-    <script>
-      window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-    </script>
-</html>`
-      );
+      res.status(200).send(html);
     }
   });  
 
---
+++ src/server/renderInitialApp.js
@@ -0,0 +1,70 @@
+import { createStore } from 'redux';
+import { StaticRouter } from 'react-router-dom';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import React from 'react';
+
+import models from "./models";
+import mythicPlusApp from '../store/rootReducer';
+import App from "../App";
+
+export const getContext = () => {
+  return {};
+};
+
+const createStoreForClient = async () => {
+  const store = createStore(mythicPlusApp);
+  const context = getContext();
+  const dungeons = await models.Dungeon.getAll();
+  const initialState = store.getState();
+
+  initialState.dungeons = {
+    dungeons,
+    selected: {},
+    keyLevel: null,
+  };
+
+  return { store, context, initialState };
+};
+
+const createMarkup = async (req) => {
+  const { store, context, initialState } = await createStoreForClient();
+  const markup = renderToString(
+    <Provider store={store}>
+      <StaticRouter context={context} location={req.url}>
+        <App />
+      </StaticRouter>
+    </Provider>
+  );
+  return {markup, initialState};
+};
+
+export const createHTML = async (assets, req) => {
+  const { markup, initialState } = await createMarkup(req);
+
+  return `<!doctype html>
+    <html lang="">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charset="utf-8" />
+        <title>Welcome to Razzle</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${
+          assets.client.css
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''
+        }
+        ${
+          process.env.NODE_ENV === 'production'
+            ? `<script src="${assets.client.js}" defer></script>`
+            : `<script src="${assets.client.js}" defer crossorigin></script>`
+        }
+    </head>
+    <body>
+        <div id="root">${markup}</div>
+    </body>
+    <script>
+      window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+    </script>
+</html>`
+};
